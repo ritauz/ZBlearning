860b3b192c8b9074b92f57783e1031716514acf6
ritauz <rok20010814@gmail.com>
2025-07-06 20:52:39 +0900

    🔧 44 開発環境の構築

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from FastAPI"}
+
+handler = Mangum(app)
+
+# ローカル開発用の起動
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
